3783968c5a62d20f0394b3094292c50df9df22bd
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2024-02-21 18:02:34 +0100

    finished without bonuses
    learnt about the redirected method of the res object from a fetch api and learnt about URlSearchParams on a basic level

--- fetch-requests.js
+++ fetch-requests.js
@@ -6,15 +6,52 @@ Paste your code for fetch requests here once you finish each phase.
 /* ============================== Phase 1 ============================== */
 
 // Your code here
+fetch("./products", {
+    method: "POST",
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+})
 
 
 
 /* ============================== Phase 2 ============================== */
 
 // Your code here
+fetch('/products', {
+    method: 'POST',
+    body: "name=Caribbean+Delight+Coffee&description=Made+by+Manatee+Coffee&price=11%2E99&categories=grocery",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+})
+    .then(res => {
+        console.log(`response status code = ` + res.status);
+        console.log(`reponse Content-type of header = ` + res.headers.get('Content-type'));
+        console.log(`response URL = ` + res.url);
+        console.log(`response .redirected = ` + res.redirected);
+       
+    });
+// redirected is true, and URL is URL redirected.
+// but status code is still 200, not 302
+
 
 
 
 /* ============================== Phase 3 ============================== */
 
 // Your code here
+const formData = new URLSearchParams();
+formData.append("name", "Caribbean Delight Coffee");
+formData.append("description", "Made by Manatee Coffee");
+formData.append("price", "11.99");
+formData.append("categories", "grocery");
+
+fetch("./products", {
+    method: "POST",
+    body: formData,
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+})
